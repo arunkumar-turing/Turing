7401c7fbd79323047bb06ef6b8ef8a7b78e02488
arunkumar-turing <arunkumar.r@turing.com>
2025-01-20 13:35:05 +0530

    added the metadataExtractor

---
+++ 512350/Turn1/modelB/metadataExtractor/metadataExtractor.go
@@ -0,0 +1,33 @@
+package metadataExtractor
+
+import (
+	"os"
+	"time"
+)
+
+type FileInfo struct {
+	Size    int64
+	Type    string
+	ModTime time.Time
+}
+
+// GetFileInfo extracts metadata from an opened file.
+func GetFileInfo(file *os.File) (*FileInfo, error) {
+	stat, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	var fileType string
+	if stat.IsDir() {
+		fileType = "directory"
+	} else {
+		fileType = "file"
+	}
+
+	return &FileInfo{
+		Size:    stat.Size(),
+		Type:    fileType,
+		ModTime: stat.ModTime(),
+	}, nil
+}
