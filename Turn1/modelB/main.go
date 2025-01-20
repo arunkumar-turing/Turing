1e2f035bfec691d719e59a5e684d9b93326afdea
arunkumar-turing <arunkumar.r@turing.com>
2025-01-20 13:32:43 +0530

    added the metadataExtractor

---
+++ Turn1/modelB/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/shahrukhq/metadata-extractor/metadataExtractor"
+)
+
+func main() {
+	// Check if the number of arguments is correct
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run main.go <file_path>")
+		os.Exit(1)
+	}
+
+	filePath := os.Args[1]
+	metadata, err := extractMetadata(filePath)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	fmt.Printf("File Path: %s\n", filePath)
+	fmt.Printf("File Size: %d bytes\n", metadata.Size)
+	fmt.Printf("File Type: %s\n", metadata.Type)
+	fmt.Printf("Modification Time: %s\n", metadata.ModTime)
+}
+
+func extractMetadata(filePath string) (*metadataExtractor.FileInfo, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	metadata, err := metadataExtractor.GetFileInfo(file)
+	return metadata, err
+}
