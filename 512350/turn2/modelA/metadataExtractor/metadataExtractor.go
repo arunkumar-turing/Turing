99774fc9cac499f18c509d93a7f3315cf6c6e807
arunkumar-turing <arunkumar.r@turing.com>
2025-01-24 17:03:33 +0530

    Update metadataExtractor.go

--- 512350/turn2/modelA/metadataExtractor/metadataExtractor.go
+++ 512350/turn2/modelA/metadataExtractor/metadataExtractor.go
@@ -1,46 +1,35 @@
 package metadataExtractor
 
 import (
-	"os"
-	"time"
+    "os"
+    "time"
 )
 
 type FileInfo struct {
-	Size        int64
-	Type        string
-	ModTime     time.Time
-	Permissions os.FileMode
-	Hidden      bool
+    Size    int64
+    Type    string
+    ModTime time.Time
+    Mode    os.FileMode
 }
 
 // GetFileInfo extracts metadata from an opened file.
 func GetFileInfo(file *os.File) (*FileInfo, error) {
+    stat, err := file.Stat()
+    if err != nil {
+        return nil, err
+    }
 
-	stat, err := file.Stat()
-	if err != nil {
-		return nil, err
-	}
+    var fileType string
+    if stat.IsDir() {
+        fileType = "directory"
+    } else {
+        fileType = "file"
+    }
 
-	var fileType string
-	if stat.IsDir() {
-		fileType = "directory"
-	} else {
-		fileType = "file"
-	}
-
-	// Determine if the file is hidden
-	isHidden := isFileHidden(file.Name())
-
-	return &FileInfo{
-		Size:        stat.Size(),
-		Type:        fileType,
-		ModTime:     stat.ModTime(),
-		Permissions: stat.Mode().Perm(), // Get file permissions
-		Hidden:      isHidden,
-	}, nil
-}
-
-// isFileHidden checks if a file is hidden based on its name.
-func isFileHidden(name string) bool {
-	return len(name) > 1 && name[0] == '.' // Checks if file name starts with '.'
+    return &FileInfo{
+        Size:    stat.Size(),
+        Type:    fileType,
+        ModTime: stat.ModTime(),
+        Mode:    stat.Mode(),
+    }, nil
 }
